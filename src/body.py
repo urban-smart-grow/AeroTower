60e8c1f65c999674871def274a648707acce37f2
Malte Fillmer <malte.fillmer@gmail.com>
2023-01-02 21:35:11 +0000

    start body

--- src/body.py
+++ src/body.py
@@ -7,22 +7,58 @@ diameter = 160
 height = 180
 
 
+def locate_cone(loc: Location):
+    x, y, z = loc.toTuple()[0]
+
+    return (
+        cq.Solid.makeCone(
+            plant_cup.bottom_diameter/2,
+            plant_cup.top_diameter/2,
+            plant_cup.height
+        )
+        .rotate(Vector(0, 0, 0), Vector(-y, x, 0), 45)
+        .locate(loc)
+    )
+
+
+def locate_socket(loc: Location):
+    x, y, z = loc.toTuple()[0]
+
+    return (
+        cq.Solid
+        .makeCone(
+            plant_cup.bottom_diameter/2 + wall,
+            plant_cup.top_diameter/2 + wall,
+            plant_cup.height
+        )
+        .cut(
+            cq.Solid
+            .makeCone(
+                plant_cup.bottom_diameter/2,
+                plant_cup.top_diameter/2,
+                plant_cup.height
+            )
+        )
+        .rotate(Vector(0, 0, 0), Vector(-y, x, 0), 45)
+        .locate(loc)
+    )
+
+
 body = (
     cq.Workplane('XY')
     .circle(diameter/2)
     .circle(diameter/2-wall)
     .extrude(height)
-    .workplane()
-    .pushPoints(calculate_circle_points(5, 80))
-    .cutEach(
-        lambda loc:
-        cq.Solid.makeCone(
-            plant_cup.bottom_diameter/2,
-            plant_cup.top_diameter/2,
-            plant_cup.height
-        ).rotate(Vector(0, 0, 0), Vector(1, 1, 0), 45)
-        .locate(loc)
+    .workplane(offset=-20)
+    .pushPoints(calculate_circle_points(5, height/3))
+    .cutEach(locate_cone)
+    .pushPoints(calculate_circle_points(5, height/3))
+    .eachpoint(locate_socket, combine='a')
+    .combine()
+    .intersect(
+        cq.Solid.makeCylinder(diameter/2, height)
     )
-
 )
+
+
 exporters.export(body, './exports/body.stl')
